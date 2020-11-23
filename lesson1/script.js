19f78cfb70c00ea42e470f8a4e36f9ee0f57cee4
Alexandr Babkin <alexandr.babkin2006@gmail.com>
2020-11-23 14:51:25 +0600

    Первый коммит

--- lesson1/script.js
+++ lesson1/script.js
@@ -8,13 +8,13 @@
 
 //var let const - переменные
 console.log("Hello world");
-
+alert("Hello friends");
 //let agePerson = 20;
 //console.log('agePerson: ', agePerson);
 
-const money = 0;
-const income = 9;
-var addExpenses = 4;
-let deposit = 3;
-let mission = 4;
+let money; 
+let income;
+var addExpenses;
+let deposit;
+let mission;
 
