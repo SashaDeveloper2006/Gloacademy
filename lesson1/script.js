72b13c48e9c5a969bbaf7bc0e38c2d8c6b452e7d
Alexandr Babkin <alexandr.babkin2006@gmail.com>
2020-11-23 11:20:39 +0600

    Первый коммит

---
+++ lesson1/script.js
@@ -0,0 +1,20 @@
+// komment
+
+//  console.log(3); - 
+//  console.log(2);
+
+//var myVar = 10;
+//var camelCase = 10;
+
+//var let const - переменные
+console.log("Hello world");
+
+//let agePerson = 20;
+//console.log('agePerson: ', agePerson);
+
+const money = 0;
+const income = 9;
+var addExpenses = 4;
+let deposit = 3;
+let mission = 4;
+
---
+++ lesson2homeWork/script.js
@@ -0,0 +1,21 @@
+let money = 30000;
+console.log(typeof money);
+
+let income = 'Фриланс=10000';
+console.log(typeof income);
+
+let addExpenses = 'Квартира, метро, еда';
+console.log(addExpenses.split());
+console.log(addExpenses.length);
+console.log(addExpenses.toLowerCase());
+
+let deposit = 5000;
+console.log(Boolean(5000));
+
+let mission = 500000;
+let period = 11;
+console.log('Период равен', period, 'месяцев.', 'Цель заработать', mission, 'руб');
+
+let budgetDay;
+budgetDay = 40000;
+console.log(budgetDay);
