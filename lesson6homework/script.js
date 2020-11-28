fefd459e33c3b472d689d4d41179c865b617b8c1
Alexandr Babkin <alexandr.babkin2006@gmail.com>
2020-11-28 23:25:34 +0600

    Поправил 6 задание

--- lesson6homework/script.js
+++ lesson6homework/script.js
@@ -8,19 +8,24 @@ let score = function(){
     console.log(botNum);
     let num = prompt('Угадайте число от 1 до 100');
 
+    let result = function(){
+
     while (isNaN(num) || num.trim() === '') {
         num = prompt('Введите число');
     }
     
         if (num === botNum) {
-        prompt('Поздравляю, Вы угадали');
+        prompt(Number('Поздравляю, Вы угадали'));
         } else if (num > botNum) {
-        prompt('Загаданное число меньше');
+        prompt(Number('Загаданное число меньше'));
         } else if (num < botNum) {
-        prompt('Загаданное число больше');
+        prompt(Number('Загаданное число больше'));
        } else if (num === null) {
-        prompt('Игра окончена');
+        prompt(Number('Игра окончена'));
       }
+      return result;
+    };
+    result();
    
 };
 
