a605395f86e731c11005290767bb05f5ef749699
Alexandr Babkin <alexandr.babkin2006@gmail.com>
2020-12-01 15:05:09 +0600

    6 задание исправленнор

--- lesson6homework/script.js
+++ lesson6homework/script.js
@@ -14,7 +14,6 @@ let score = function(){
     
         if (+num === botNum) {
         alert('Поздравляю, Вы угадали');
-        result();
         } else if (num > botNum) {
         alert('Загаданное число меньше');
         result();
